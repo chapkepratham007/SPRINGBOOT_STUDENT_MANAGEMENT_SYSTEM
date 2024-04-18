726775bc5167f937b3dafd1a6bad745619082037
pratham <chapkepratham@gmail.com>
2024-04-18 21:00:22 +0530

    spring

---
+++ STUDENT_MANAGEMENT/src/main/java/com/example/STUDENT_MANAGEMENT/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.STUDENT_MANAGEMENT.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.STUDENT_MANAGEMENT.entity.Student;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+}
